6a4a19b2884460c34ae10c648c854e27b1f9af68
surya <72649807+surya2505-source@users.noreply.github.com>
2021-02-10 13:50:22 +0530

    Add files via upload

---
+++ build/precache-manifest.ae11a6ea5187bf760b24734ae2f08a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cc1e09453510fec941fdf3149b359d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f51afe774744370528a8",
+    "url": "/static/css/main.d23a063f.chunk.css"
+  },
+  {
+    "revision": "c09dfd46bfd0af2599f4",
+    "url": "/static/js/2.974e532c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.974e532c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51afe774744370528a8",
+    "url": "/static/js/main.4c1112a3.chunk.js"
+  },
+  {
+    "revision": "64f7801fd5703aaae4d2",
+    "url": "/static/js/runtime-main.c7283e59.js"
+  }
+]);
